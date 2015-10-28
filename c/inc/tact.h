33c20d331f5eb51c28a9979616eb55fa4bed837c
Work_desktop <creator83@yandex.ru>
2015-10-28 15:00:30 +0300

    C files

---
+++ c/inc/tact.h
@@ -0,0 +1,14 @@
+#include "MK02F12810.h"                 // Device header
+
+#define F_CPU 8000000
+
+#ifndef TACT_H
+#define TACT_H
+
+void fll_init (void);
+
+void FEI_init (void);
+
+void FEE_init (void);
+
+#endif
--- c/prj/use_uart.c
+++ c/prj/use_uart.c
@@ -1,9 +1,11 @@
 #include "MK02F12810.h"                 // Device header
+#include "tact.h"
 #include "uart.h"
 
 
 int main ()
 {
+	
 	init_uart0();
 	while (1)
 	{
