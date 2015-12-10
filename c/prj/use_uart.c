b50e79af42958e673b7ee8d5d87780fbde201eb8
Work_desktop <creator83@yandex.ru>
2015-12-10 17:47:48 +0300

    10_12_15

--- c/prj/use_uart.c
+++ c/prj/use_uart.c
@@ -2,6 +2,30 @@
 #include "tact.h"
 #include "uart.h"
 
+#define SCK  17
+#define MOSI 18
+#define MISO 19
+
+#define MASTER 31
+#define CPOL 26
+#define CPHA 25
+
+void init_spi (void)
+{
+	
+	//init pins
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	PORTE->PCR [SCK] |= PORT_PCR_MUX(2);
+	PORTE->PCR [MOSI] |= PORT_PCR_MUX(2);
+	PORTE->PCR [MISO] |= PORT_PCR_MUX(2);
+	PTE->PDDR |= 1 << SCK;
+	PTE->PDDR |= 1 << MOSI;
+	
+	//init spi
+	SIM->SCGC6 |= SIM_SCGC6_SPI0_MASK;
+	SPI0->MCR |= 1 << 31;
+	//SPI0->CTAR |= 
+}
 
 int main ()
 {
